6de67317e8ecf4dc98046e22ff1eb8b7a2e6b01b
Pedro Delmonte <pdelmonte@gmail.com>
2017-03-06 13:17:20 +0100

    Added landscape fragments and update button to first fragment with random number generation.

--- app/src/main/java/org/bts/android/dummyfragment/FirstFragment.java
+++ app/src/main/java/org/bts/android/dummyfragment/FirstFragment.java
@@ -1,18 +1,28 @@
 package org.bts.android.dummyfragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener{
 
+    public interface ICom2Activity {
+        public void getRandomNumberFromFragment1();
+    }
+
+    private ICom2Activity mInterfaceInstance;
+
+    private static final String TAG = FirstFragment.class.getName();
 
     public FirstFragment() {
         // Required empty public constructor
@@ -23,7 +33,34 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_first, container, false);
+
+        final Button btnUpdateSecondFrag = (Button) rootView.findViewById(R.id.btn_update_fragment2);
+        btnUpdateSecondFrag.setOnClickListener(this);
+
+        return rootView;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            this.mInterfaceInstance = (ICom2Activity) context;
+        } catch (ClassCastException e) {
+            Log.e(FirstFragment.TAG, "Error on casting context");
+        }
+    }
+
+    @Override
+    public void onClick(View whichView) {
+
+        Log.i(FirstFragment.TAG, "Button Clicked!");
+
+        if (mInterfaceInstance != null) {
+            mInterfaceInstance.getRandomNumberFromFragment1();
+        }
+
+    }
 }
--- app/src/main/java/org/bts/android/dummyfragment/MainActivity.java
+++ app/src/main/java/org/bts/android/dummyfragment/MainActivity.java
@@ -1,15 +1,108 @@
 package org.bts.android.dummyfragment;
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, FirstFragment.ICom2Activity{
 
-    private static final String TAG1 = MainActivity.class.getSimpleName();
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private FrameLayout mFragmentContainer;
+    private View mMainLayout;
+    private boolean mTwoPane;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String currentOrientation = getResources().getConfiguration()
+                .orientation == Configuration.ORIENTATION_LANDSCAPE ?
+                "Landscape":
+                "Portrait";
+        Log.i(MainActivity.TAG, "Orientation: " + currentOrientation);
+
+        this.mTwoPane = getResources().getConfiguration()
+                .orientation != Configuration.ORIENTATION_LANDSCAPE;
+
+        if (this.mTwoPane){
+            this.mFragmentContainer = (FrameLayout) findViewById(R.id.frame_layout_container_activity_main);
+            this.mMainLayout = findViewById(R.id.linear_layout_activity_main);
+            this.mMainLayout.setOnClickListener(this);
+
+
+
+        } else {
+            final Button btnSwapFrag = (Button) findViewById(R.id.btn_swap_fragment);
+            btnSwapFrag.setOnClickListener(this);
+
+            FragmentManager fragManager = this.getSupportFragmentManager();
+            fragManager.beginTransaction()
+                .replace(R.id.frame_layout_container_activity_main_land, new FirstFragment(), "First Fragment Landscape")
+                .commit();
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View whichView) {
+        if (whichView.getId() == R.id.linear_layout_activity_main) {
+
+            Log.i(MainActivity.TAG, "Main Layout clicked!");
+
+            FragmentManager fragManager = this.getSupportFragmentManager();
+            FragmentTransaction fragTransaction = fragManager.beginTransaction();
+
+            fragTransaction.replace(R.id.frame_layout_container_activity_main, new FirstFragment(), "First Fragment");
+            fragTransaction.commit();
+
+        } else if(whichView.getId() == R.id.btn_swap_fragment){
+
+            FragmentManager fragManager = this.getSupportFragmentManager();
+
+            boolean isFirstFragment = fragManager.findFragmentByTag("First Fragment Landscape") != null;
+
+            Fragment fragment;
+            String fragmentTag;
+            int color;
+
+            if(isFirstFragment) {
+                fragment = new SecondFragment();
+                fragmentTag = "Second Fragment Landscape";
+                color = R.color.colorYellow;
+
+            } else {
+                fragment = new FirstFragment();
+                fragmentTag = "First Fragment Landscape";
+                color = R.color.colorGreen;
+            }
+
+            fragManager.beginTransaction()
+                    .replace(R.id.frame_layout_container_activity_main_land, fragment, fragmentTag)
+                    .commit();
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout_bottom_activity_main_land);
+            linearLayout.setBackgroundColor(ContextCompat.getColor(this, color));
+        }
+    }
+
+    @Override
+    public void getRandomNumberFromFragment1() {
+
+        double randomNumber = Math.random();
+
+        Log.i(MainActivity.TAG, "Random Number Generated: " + randomNumber);
+
+        //Fragment fragment = findViewById(R.id.fragment_main_activity);
     }
 }
